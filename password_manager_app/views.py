6720273d552c18079a198b74974ee37abe921996
Yaroslav <arhdomain@gmail.com>
2022-03-03 11:44:19 +0300

    optimize

--- password_manager_app/views.py
+++ password_manager_app/views.py
@@ -28,9 +28,9 @@ def get_tag(request, tag):
     tagname = get_object_or_404(Tag, slug=tag)
     pass_list = PasswordManager.objects.all()
     tags_list = Tag.objects.all()
-    paginator = Paginator(pass_list, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    # paginator = Paginator(pass_list, 10)
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
     context = {
         'pass_list': pass_list,
         'tags_list': tags_list,
